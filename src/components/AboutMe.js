8d9d899cbbbdefea0231303d37a3cca1a65e7cf8
PittamHarika <pittamharika04@gmail.com>
2023-10-15 16:58:42 +0530

    first commit

---
+++ src/components/AboutMe.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <section>
+            <h2>About Me</h2>
+            <p>
+                Hi there! I'm, a dedicated and enthusiastic web developer.I find it very intresting. I am very passion towards my intrest. I am person willing to work hard.
+            </p>
+
+        </section>
+    );
+};
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <section>
+            <h2>Contact Me</h2>
+            <ul>
+                <li>
+                    <strong>Email:</strong> <a href=" ">Email</a>
+                </li><br />
+
+                <li>
+                    <strong>LinkedIn:</strong> <a href=" " target="_blank" rel="noopener noreferrer">Linkedin</a>
+                </li>
+                <br />
+                <li>
+                    <strong>GitHub:</strong> <a href=" " target="_blank" rel="noopener noreferrer">Github</a>
+                </li>
+
+            </ul>
+            <p>
+                I will always welcome you to Contact.<br />
+                I'm looking forward to connecting with you!
+            </p>
+        </section>
+    );
+};
+
+export default Contact;
